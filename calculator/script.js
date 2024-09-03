c634da9013a282e5c120fd2288a7e5dc972b57c8
Mayer Iqbal <166912010+mayeriqbal@users.noreply.github.com>
2024-09-03 21:22:36 +0500

    UPDATE CALCULATOR

--- calculator/script.js
+++ calculator/script.js
@@ -1,6 +1,6 @@
 let string = "";
 let buttons = document.querySelectorAll(".button");
-Array.from(buttons).forEach((button)=>{
+Array.from(buttons).forEach((button) =>{
    button.addEventListener('click', (e)=>{
     console.log(e.target)
     string = string + e.target.innerHtml;
