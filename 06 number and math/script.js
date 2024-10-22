b3654090b1a26a1c3048b99600e36ab1db0d49c5
Mayer Iqbal <mayeriqbal03@gmail.com>
2024-10-22 22:35:11 +0500

    6 math and number

---
+++ 06 number and math/script.js
@@ -0,0 +1,78 @@
+// const score = 400
+// // console.log(score);
+
+// const balance = new Number(100)
+// // console.log(balance);
+
+// // console.log(balance.toString().length);
+// // console.log(balance.toFixed(1));
+
+// const otherNumber = 123.8966
+
+// // console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 1000000
+// // console.log(hundreds.toLocaleString('en-IN'));
+
+// // +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// // console.log(Math);
+// // console.log(Math.abs(-4));
+// // console.log(Math.round(4.6));
+// // console.log(Math.ceil(4.2));
+// // console.log(Math.floor(4.9));
+// // console.log(Math.min(4, 3, 6, 8));
+// // console.log(Math.max(4, 3, 6, 8));
+
+// // console.log(Math.random());
+// // console.log((Math.random()*10) + 1);
+// // console.log(Math.floor(Math.random()*10) + 1);
+
+// // const min = 10
+// // const max = 20
+
+// // console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+// var name = "Ahsan Iqbal";
+// name = "Mayer Iqbal";
+// console.log(name);
+// 
+// const number = "50";
+
+// if (number => "33") {
+//   let name = "Ahsan";
+//   name = "Mayer";
+//   console.log(name);
+// }
+// name = "Phatoo Dinga";
+// console.log(name)
+
+const arr = ['ahsan','ahmad','ali', 'usama']
+console.log(arr)
+
+const number = [
+{
+    name : "mayer",
+    email : "mayeriqbal01@gmail.com",
+    phoneNumber : "03070029291"
+},
+
+
+    {
+        name : "anns",
+        email : "annsiqbal01@gmail.com",
+        phoneNumber : "03070029291"
+    },
+    
+
+    {
+            name : "ahsan",
+            email : "ahsaniqbal01@gmail.com",
+            phoneNumber : "03070029291"
+    }
+    ]
+    console.log(array)
+    const newarr = number.map
+
+    function myFunction(num)
+
