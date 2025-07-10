d8bcc76e1a7c70482a3f2ee1ed9fb24086e808ce
Mayer Iqbal <mayeriqbal03@gmail.com>
2025-07-10 22:12:30 +0500

    Es6 modules

---
+++ module/appM.js
@@ -0,0 +1,8 @@
+//default import
+import multiply from "./mathoperationM,js";
+
+// named import
+import { add, subtract } from './mathoperationM.js'
+console.log(multiply(2, 2));
+console.log(add(2, 2));
+console.log(subtract(2, 2));
